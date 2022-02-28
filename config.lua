d258211897718318968c73e5e2153ca6b411d8c1
IceBreak <shanesmith1801@gmail.com>
2022-02-28 01:05:58 +0000

    added stashes for AE bennys and vagos

--- config.lua
+++ config.lua
@@ -2,4 +2,7 @@ Config = {}
 Config.StashCoords ={
     [1] = {coords = vector3(-66.9147, -1595.79, 29.715), name = "test5", job = {}},
     [2] = {coords = vector3(-64.5573, -1596.99, 29.599), name = "test6", job = {"police", "mechanic"}}
+    [3] = {coords = vector3(547.2, -182.96, 54.49), name = "autoexoticstash", job = {"autoexotic"}},
+    [4] = {coords = vector3(-37.72, -1039.43, 28.6), name = "bennysstash", job = {"bennys"}},
+    [5] = {coords = vector3(974.28, -1839.17, 36.11), name = "vagosstash", job = {"vagos"}},
 }
